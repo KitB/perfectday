3ad652731fc3f1380e68610acd6e8e40f0a756a2
Kit Barnes <kit@ninjalith.com>
2016-08-02 23:19:17 +0100

    Tell genprot where to find its stuff

--- genprot.sh
+++ genprot.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python -m grpc.tools.protoc -I . --python_out=perfectday --grpc_python_out=perfectday protos/*.proto
+python -m grpc.tools.protoc -I protos --python_out=perfectday --grpc_python_out=perfectday protos/*.proto
