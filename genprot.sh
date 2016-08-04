4135bc9fd59ce682c2402dd876165006ed6c35f1
Kit Barnes <kit@ninjalith.com>
2016-08-04 23:32:19 +0100

    Make genprot work from any directory
    (hopefully)

--- genprot.sh
+++ genprot.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+cd $dir
 python -m grpc.tools.protoc -I protos --python_out=python/perfectday --grpc_python_out=python/perfectday protos/*.proto
