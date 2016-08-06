690ffb911c69e512720e338f1ce3d51526db95ef
Kit Barnes <kit@ninjalith.com>
2016-08-06 02:16:29 +0100

    Make the test module handier

--- python/test.py
+++ python/test.py
@@ -24,10 +24,29 @@ def fill_test_user(proto):
     pa.every = 1
     pa.starts.GetCurrentTime()
 
+    r = p.regulars.add()
+    r.id = 2
+    r.name = "Some other thing"
+    r.description = "Plinky ploink"
+    r.weight = 1
+
+    pa = r.schedule.pattern.add()
+    pa.every = 7
+    pa.starts.GetCurrentTime()
+
+    pa = r.schedule.pattern.add()
+    pa.every = 2
+    pa.starts.GetCurrentTime()
+
+
 if __name__ == '__main__':
     import IPython
-    channel = grpc.insecure_channel('localhost:9051')
-    store = client.GRPCStore(channel)
-    model.BaseStorable.set_store(store)
+
+    def connect():
+        channel = grpc.insecure_channel('localhost:9051')
+        store = client.GRPCStore(channel)
+        model.BaseStorable.set_store(store)
+
+    connect()
 
     IPython.embed()
